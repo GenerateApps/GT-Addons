c5f3df5fa588e87a88e91296449dc466ef175954
chippytech <80016011+chippytech@users.noreply.github.com>
2023-03-11 11:20:47 -0600

    Rename homepage.php to selector/homepage.php

--- homepage.php
+++
@@ -1,7 +0,0 @@
-
-
- <h1><?php echo htmlspecialchars($gcmdname); ?></h1>
-  <p>Select a version of <?php echo htmlspecialchars($gcmdname); ?> to run:</p> 
-<?php include "links.php" ?>
-
-
---
+++ selector/homepage.php
@@ -0,0 +1,12 @@
+
+
+
+ <h1><?php echo htmlspecialchars($gcmdname); ?></h1>
+
+  <p>Select a version of <?php echo htmlspecialchars($gcmdname); ?> to run:</p> 
+
+<?php include "links.php" ?>
+
+
+
+
