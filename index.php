19c10fb66970b7bb3e7fb414ee71cd43f6985a8e
chippytech <80016011+chippytech@users.noreply.github.com>
2023-03-11 11:06:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,83 @@
-<?php
-include 'config.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<?php 
-if( !isset($_POST['name'])){
-	$title = "Select version";
-
-} else {
-	$title = "Form submitted";
-}
-?>
-
-  <title><?php
-echo htmlspecialchars($gcmdname) . " - $title";
-?></title>
-  <meta charset="utf-8">
-
+<?php
 
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-</head>
-<body>
-
+include 'config.php';
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<?php 
+
+if( !isset($_POST['name'])){
+
+	$title = "Select version";
 
 
-  
- 
-<?php 
-if( !isset($_POST['name'])){
-include 'homepage.php';
-
-} else {
-	include 'result.php'; 
-}
-echo "$gcmdadcode";
-?>
-<footer>  
-<?php 
-$year = date("Y"); 
-echo "Copyright $year $gcmdowner. ";
-?> 
-</footer>
-</body>
+
+} else {
+
+	$title = "Form submitted";
+
+}
+
+?>
+
+
+
+  <title><?php
+
+echo htmlspecialchars($gcmdname) . " - $title";
+
+?></title>
+
+  <meta charset="utf-8">
+
+
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+
+</head>
+
+<body>
+
+
+
+
+
+  
+
+ 
+
+<?php 
+
+
+
+include 'homepage.php';
+
+
+echo "$gcmdadcode";
+
+?>
+
+<footer>  
+
+<?php 
+
+$year = date("Y"); 
+
+echo "Copyright $year $gcmdowner. ";
+
+?> 
+
+</footer>
+
+</body>
+
 </html>
